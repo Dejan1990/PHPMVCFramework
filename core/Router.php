49a1b730ee0b5c74242c7b8811ffceefbe76266f
Dejan1990 <dejan.simo@hotmail.com>
2023-06-07 10:33:19 +0200

    Create get and run methods

--- core/Router.php
+++ core/Router.php
@@ -3,5 +3,15 @@
 namespace app\core;
 class Router 
 {
+    protected $routes = [];
 
+    public function get($path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function resolve()
+    {
+        var_dump($_SERVER);
+    }
 }
